8de2efc994ac969b13ac0df42f043553d5d61701
antoniotto71 <antoniotto71@yahoo.it>
2022-10-24 22:07:19 +0200

    Create hashed_password

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,8 +6,17 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :comments
 
+  scope :published, -> { where.not(published_at: nil ) }
+  scope :draft, -> { where(published_at: nil) }
+  scope :recent, -> { published.where('published_at > ?', 2.weeks.ago.to_date) } #correction from book
+  scope :where_title, -> (term) { where('title LIKE ?', "%#{term}%")}
+
   def long_title
     "#{title} - #{published_at}"
+  end
+  
+  def published?
+    published_at.present?
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :name, :email, :body, presence: true
+  validate :article_should_be_published # validate not validates
+  after_create :email_article_author
+
+  def article_should_be_published
+    errors.add(:article_id, 'is not published yet') if article && !article.published?
+  end
+  def email_article_author
+    puts "We will notify #{aticle.user.email}  in chapter 12" if article
+  end
+
 end
